aab73cff9ab71973db5fab92cfc9cc6f25ebf297
KALIPE Kossi Mathias <kalipemathias4@gmail.com>
2022-04-14 18:05:28 +0000

    Just a late commit

---
+++ app/src/main/java/to/imcurious/yakate/TermsOfUseFragment.kt
@@ -0,0 +1,23 @@
+package to.imcurious.yakate
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import to.imcurious.yakate.R
+
+
+class TermsOfUseFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_terms_of_use, container, false)
+    }
+
+
+}
---
+++ app/src/main/java/to/imcurious/yakate/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package to.imcurious.yakate.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
